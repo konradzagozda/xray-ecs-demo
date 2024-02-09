34997db9dc66b699376680c75b3f2eb582c46aac
Konrad Zagozda <konrad.zagozda@grapeup.com>
2024-02-09 15:57:52 +0100

    improve script

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# usage:
-# ./build.sh 0.0.1
+# examplke usage:
+# ./build.sh 817861099197.dkr.ecr.us-west-2.amazonaws.com app-with-xray 0.0.1
 
-TAG=${1:-latest}
+
+ECR_URL=${1:-latest}
+REPO_NAME=${2:-latest}
+TAG=${3:-latest}
 
 export AWS_PROFILE=kz_wp
-docker build --platform=linux/amd64 -t app-with-xray:$TAG .
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 817861099197.dkr.ecr.us-west-2.amazonaws.com
-docker tag app-with-xray:$TAG 817861099197.dkr.ecr.us-west-2.amazonaws.com/app-with-xray:$TAG
-docker push 817861099197.dkr.ecr.us-west-2.amazonaws.com/app-with-xray:$TAG
+docker build --platform=linux/amd64 -t $REPO_NAME:$TAG .
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ECR_URL
+docker tag $REPO_NAME:$TAG $ECR_URL/$REPO_NAME:$TAG
+docker push $ECR_URL/$REPO_NAME:$TAG
